6bbe6a46c617b82126c44ea45b818a8eeca13463
amirhosein <amirhosein.oj@gmail.conm>
2024-05-18 17:23:49 +0330

    dev : second commit message

--- src/main/java/manamaham/amirhossein/kafkatask/controller/MessageController.java
+++ src/main/java/manamaham/amirhossein/kafkatask/controller/MessageController.java
@@ -2,7 +2,11 @@ package manamaham.amirhossein.kafkatask.controller;
 
 import lombok.RequiredArgsConstructor;
 import manamaham.amirhossein.kafkatask.kafka.KafkaProducer;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +16,11 @@ public class MessageController {
 
     private final KafkaProducer kafkaProducer;
 
-
+    //http:localhost:8080/api/kafka/v1/publish?message=hello world(test)
+    @GetMapping(value = "/publish")
+    public ResponseEntity<String> publish(@RequestParam("message") String message){
+        kafkaProducer.sendMessage(message);
+        ResponseEntity.BodyBuilder ok = ResponseEntity.ok();
+        return ok.body("Message Sent To THe Topic");
+    }
 }
