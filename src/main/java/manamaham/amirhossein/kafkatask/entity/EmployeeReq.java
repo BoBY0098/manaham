61a399b034be29211a0efeca4d1ecd5669455e07
amirhosein <amirhosein.oj@gmail.conm>
2024-05-19 15:15:09 +0330

    req models created

---
+++ src/main/java/manamaham/amirhossein/kafkatask/entity/EmployeeReq.java
@@ -0,0 +1,25 @@
+package manamaham.amirhossein.kafkatask.entity;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class EmployeeReq {
+
+    private Long id;
+
+    private String name;
+
+    private String department;
+
+    private String position;
+
+    private Double salary;
+
+    private Date dueDate;
+
+    List<TaskReq> reqList = new ArrayList<>();
+}
---
+++ src/main/java/manamaham/amirhossein/kafkatask/entity/TaskReq.java
@@ -0,0 +1,19 @@
+package manamaham.amirhossein.kafkatask.entity;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class TaskReq {
+
+    private Long id;
+
+    private String description;
+
+    private String status;
+
+    private Date dueDate;
+
+    private Long employeeId;
+}
